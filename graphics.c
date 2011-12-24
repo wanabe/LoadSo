4ac58ecb9ffe4a9ebafadb20eb7bb61afc7c6bd7
wanabe <s.wanabe@gmail.com>
2011-12-25 00:32:20 +0900

    hWndRgss is unnecessary.

--- graphics.c
+++ graphics.c
@@ -1,14 +1,19 @@
 #include "ext_rgss.h"
 #include "d3d9.h"
 
-HWND hWndRgss;
 LPDIRECT3DDEVICE9 pD3DDevice;
 
+struct hWndFinder {
+  HWND hWnd;
+  DWORD pId;
+};
+
 static BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lp) {
+  struct hWndFinder *pFinder = (struct hWndFinder *)lp;
   DWORD pId;
   GetWindowThreadProcessId(hWnd, &pId);
-  if(pId == lp) {
-    hWndRgss = hWnd;
+  if(pId == pFinder->pId) {
+    pFinder->hWnd = hWnd;
     return FALSE;
   }
   return TRUE;
@@ -20,8 +25,10 @@ static VALUE Graphics_s_init(VALUE self) {
   D3DPRESENT_PARAMETERS D3DPP = {0,0,D3DFMT_UNKNOWN,0,D3DMULTISAMPLE_NONE,0,
     D3DSWAPEFFECT_DISCARD,NULL,TRUE,TRUE,D3DFMT_D24S8,D3DPRESENTFLAG_DISCARD_DEPTHSTENCIL,
     D3DPRESENT_RATE_DEFAULT,D3DPRESENT_INTERVAL_DEFAULT};
+  struct hWndFinder finder;
 
-  EnumWindows(EnumWindowsProc, (LPARAM)GetCurrentProcessId());
+  finder.pId = GetCurrentProcessId();
+  EnumWindows(EnumWindowsProc, (LPARAM)&finder);
   if(FAILED(CoInitialize(NULL))) {
     return 0;
   }
@@ -29,7 +36,8 @@ static VALUE Graphics_s_init(VALUE self) {
     return 0;
   }
 
-  hr = pD3D->lpVtbl->CreateDevice(pD3D, D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWndRgss, D3DCREATE_HARDWARE_VERTEXPROCESSING, &D3DPP, &pD3DDevice);
+  hr = pD3D->lpVtbl->CreateDevice(pD3D, D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, finder.hWnd,
+                                  D3DCREATE_HARDWARE_VERTEXPROCESSING, &D3DPP, &pD3DDevice);
   if(FAILED(hr)) {
     return 0;
   }
