76cafe307a4a8059ad78c8f2a848ba615e5d960e
wanabe <s.wanabe@gmail.com>
2011-12-24 15:28:01 +0900

    do not draw anything to window.

--- ext_rgss.rb
+++ ext_rgss.rb
@@ -3,3 +3,17 @@
 if Win32API.new("ext_rgss", "Init_ext_rgss", "", "i").call == 0
   raise "ExtRgss initialize error"
 end
+class << Graphics
+  def update
+    sleep 1.0 / frame_rate
+  end
+  def transition(duration = 10, *)
+    sleep duration.to_f / frame_rate
+  end
+  def fadein(duration)
+    sleep duration.to_f / frame_rate
+   end
+  def fadeout(duration)
+    sleep duration.to_f / frame_rate
+   end
+end
