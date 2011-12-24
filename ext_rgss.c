a38fdafd22d6fe723f34add2c03440a7b7ac5966
wanabe <s.wanabe@gmail.com>
2011-12-24 09:59:43 +0900

    find rgss window.

--- ext_rgss.c
+++ ext_rgss.c
@@ -1,3 +1,22 @@
+#include "ext_rgss.h"
+
+HWND hWndRgss;
+
+static BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lp) {
+  DWORD pId;
+  GetWindowThreadProcessId(hWnd, &pId);
+  if(pId == lp) {
+    hWndRgss = hWnd;
+    return FALSE;
+  }
+  return TRUE;
+}
+
 int Init_ext_rgss() {
+  RECT rect;
+
+  EnumWindows(EnumWindowsProc, (LPARAM)GetCurrentProcessId());
+  GetWindowRect(hWndRgss, &rect);
+  printf("%d %d %d %d\n", rect.left, rect.top, rect.right, rect.bottom);
   return 1;
 }
