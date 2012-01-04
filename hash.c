fac818d61b6a6128200859b97c2794f3a811e87e
wanabe <s.wanabe@gmail.com>
2012-01-04 13:09:32 +0900

    rb_hash_aref and rb_hash_aset added.

--- hash.c
+++ hash.c
@@ -1,6 +1,9 @@
 #include "load_so.h"
 #include "st.h"
 
+static VALUE (*rb_hash_aref_)(VALUE, VALUE);
+static VALUE (*rb_hash_aset_)(VALUE, VALUE, VALUE);
+
 const struct st_hash_type *ptr_objhash;
 
 VALUE rb_hash_new() {
@@ -14,6 +17,16 @@ struct st_table *rb_hash_tbl(VALUE hash) {
   return RHASH(hash)->ntbl;
 }
 
+VALUE rb_hash_aref(VALUE hash, VALUE key) {
+  return rb_hash_aref_(hash, key);
+}
+
+VALUE rb_hash_aset(VALUE hash, VALUE key, VALUE val) {
+  return rb_hash_aset_(hash, key, val);
+}
+
 void Init_Hash() {
   ptr_objhash = RHASH(rb_class_new_instance(0, NULL, rb_cHash))->ntbl->type;
+  rb_hash_aref_ = get_instance_method(rb_cHash, "[]");
+  rb_hash_aset_ = get_instance_method(rb_cHash, "[]=");
 }
