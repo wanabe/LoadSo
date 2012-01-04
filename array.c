7cf720a89d49041a499e24475ec6fe06a2a42133
wanabe <s.wanabe@gmail.com>
2012-01-04 13:11:41 +0900

    rb_ary_concat added.

--- array.c
+++ array.c
@@ -3,6 +3,7 @@
 static VALUE (*rb_ary_push_m)(int, VALUE*, VALUE);
 static VALUE (*rb_ary_join_m)(int, VALUE*, VALUE);
 static VALUE (*rb_ary_clear_)(VALUE);
+static VALUE (*rb_ary_concat_)(VALUE, VALUE);
 
 VALUE rb_ary_new() {
   return rb_class_new_instance(0, NULL, rb_cArray);
@@ -58,8 +59,13 @@ VALUE rb_ary_clear(VALUE ary) {
   return rb_ary_clear_(ary);
 }
 
+VALUE rb_ary_concat(VALUE x, VALUE y) {
+  return rb_ary_concat_(x, y);
+}
+
 void Init_Array() {
   rb_ary_push_m = get_instance_method(rb_cArray, "push");
   rb_ary_join_m = get_instance_method(rb_cArray, "join");
   rb_ary_clear_ = get_instance_method(rb_cArray, "clear");
+  rb_ary_concat_ = get_instance_method(rb_cArray, "concat");
 }
