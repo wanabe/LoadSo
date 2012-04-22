d1f8070d7f9f19ebef6b151647b727417060ec90
wanabe <s.wanabe@gmail.com>
2012-04-22 23:40:34 +0900

    replaced Kernel#require to load .rb/.so.

--- load_so.rb
+++ load_so.rb
@@ -1,5 +1,28 @@
 # encoding: utf-8
 Win32API.new("msvcrt-ruby191", "Init_LoadSo", "ii", "i").call(method(:method).object_id * 2, Object.object_id * 2)
-def require_so(name)
-  load_so "#{name}.so", "Init_#{name}"
+def require_so(file)
+  dir = File.dirname(file)
+  name = File.basename(file, ".so")
+  load_so "#{dir}/#{name}.so", "Init_#{name}"
 end
+alias require_rb require
+def require(name)
+  type = nil
+  name.gsub!(/\.(so|rb)$/) do
+    type = $1.intern
+    ''
+  end
+  $:.each do |dir|
+    if type != :rb
+      file = "#{dir}/#{name}.so"
+      return require_so(file) if File.exist?(file)
+    end
+    if type != :so
+      file = "#{dir}/#{name}.rb"
+      return require_rb(file) if File.exist?(file)
+    end
+  end
+  require_rb(name) # raise LoadError
+end
+$:.push "./Data/lib", "./Data/ext"
+$:.uniq!
